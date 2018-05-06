0a55fe08222fba576b184f200c5ebba8dcd4f095
shyamsivaraman <shyamsivaraman@gmail.com>
2018-05-06 12:34:37 +0530

    CenterFilledSquare documentation

--- SamplePrograms/src/CenterFilledSquare.scala
+++ SamplePrograms/src/CenterFilledSquare.scala
@@ -1,5 +1,15 @@
 //Classes are not runnable using main, hence the object
 
+/**
+  * Print console values as:
+  * 55555
+  * 55155
+  * 55255
+  * 55355
+  * 55455
+  * 55555
+  */
+
 object CenterFilledSquare {
 
   def main(args: Array[String]): Unit = {
@@ -7,13 +17,23 @@ object CenterFilledSquare {
   }
 
   def drawCenterFilledSquare(n: Int) = {
+    //Increase the breadth by 1 if the input is even, so that a middle
+    //column can be introduced
     val breadth = if(n%2==0) n+1 else n;
+
+    //Increase the height by 1 to compensate the extra first row having
+    //the boundary value
     val height = n+1;
+
+    //Determine the middle line where the increment values needs to be
+    //printed
     val mid = ((breadth-1)/2)+1
 
+    //Interate row by row
     for(i <- 1 to height) {
+      //Print column by column
       for(j <- 1 to breadth) {
-        if(i==0 || i==height) {
+        if(i==1 || i==height) {
           print(n);
         } else {
           if (j == mid) {
@@ -23,6 +43,7 @@ object CenterFilledSquare {
           }
         }
       }
+      //Add next line
       println("");
     }
   }
